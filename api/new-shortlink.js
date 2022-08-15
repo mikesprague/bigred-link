5500acdeb35fe30a48ceaac1d8deaa04e48c0e8f
Michael Sprague <mikesprague@gmail.com>
2022-08-15 12:54:31 +0000

    :recycle: Remove dns lookup

--- api/new-shortlink.js
+++ api/new-shortlink.js
@@ -1,5 +1,4 @@
 import Bugsnag from '@bugsnag/js';
-import dns from 'dns';
 
 import { shortenURL, handleError } from '../src/modules/api-helpers';
 
@@ -13,27 +12,25 @@ export default async (req, res) => {
   if (req.method === 'OPTIONS') {
     res.status(200).send('');
   }
+
   let originalUrl;
+
   try {
     originalUrl = new URL(req.body.link);
   } catch (error) {
     Bugsnag.notify(error);
     res.status(400).json({ error: 'Invalid URL' });
   }
-  dns.lookup(originalUrl.hostname, (err) => {
-    if (err) {
-      res.status(404).send({ error: 'URL Not Found' });
-    }
-    shortenURL(originalUrl.href)
-      .then((result) => {
-        const doc = result.value;
-        res.status(200).json({
-          original_url: doc.original_url,
-          short_id: doc.short_id,
-        });
-      })
-      .catch((error) => {
-        handleError(error);
+
+  shortenURL(originalUrl.href)
+    .then((result) => {
+      const doc = result.value;
+      res.status(200).json({
+        original_url: doc.original_url,
+        short_id: doc.short_id,
       });
-  });
+    })
+    .catch((error) => {
+      handleError(error);
+    });
 };
