ab9a2dd0f1bfd8eecc9f05ba4c1369a61f9e8122
Michael Sprague <mikesprague@gmail.com>
2022-09-21 13:39:13 +0000

    :wrench: Tweak vite pwa config

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       filename: 'service-worker.js',
       manifestFilename: 'bigred-link.webmanifest',
       workbox: {
-        navigateFallbackDenylist: [/^\/api/, /\*/],
+        navigateFallbackDenylist: [/^\/api/, /\/\w{7}/],
         clientsClaim: true,
         skipWaiting: true,
         cleanupOutdatedCaches: true,
