38a523cb03a703d73a1ce9a805f4f27326b195f3
Michael Sprague <mikesprague@gmail.com>
2022-10-14 20:22:14 +0000

    :sparkles: Add new utility script

---
+++ src/utils/back-up-db.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+import { initSupabase } from '../modules/api-helpers.js';
+
+dotenv.config();
+
+const { SUPABASE_DB_TABLE } = process.env;
+
+const { hrtime } = process;
+
+(async () => {
+  const debugStart = hrtime();
+
+  const config = {
+    fileName: 'bigred-links-db-backup.json',
+    workingDir: './',
+  };
+
+  const supabase = await initSupabase();
+
+  const getAllShortLinks = await supabase.from(SUPABASE_DB_TABLE).select();
+
+  if (getAllShortLinks.data) {
+    await fs.writeFileSync(
+      `${config.workingDir}${config.fileName}`,
+      JSON.stringify(getAllShortLinks.data, null, 2),
+    );
+  }
+
+  const debugEnd = hrtime(debugStart);
+
+  console.log(
+    `Execution time: ${debugEnd[0] * 1000 + debugEnd[1] / 1000000}ms`,
+  );
+})();
