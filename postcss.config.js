406481e192453e482cf46ffc639c8cb70c147a31
Michael Sprague <mikesprague@gmail.com>
2024-11-22 13:12:27 -0500

    chore: update config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import tailwindcss from 'tailwindcss';
+
 const cssSafelistClassArray = [
   /result-section/,
   /result/,
@@ -7,14 +12,14 @@ const cssSafelistClassArray = [
   /start-over-link/,
 ];
 
-module.exports = {
+export default {
   plugins: [
-    require('autoprefixer'),
-    require('tailwindcss'),
-    require('cssnano')({
+    autoprefixer,
+    tailwindcss,
+    cssnano({
       preset: 'default',
     }),
-    require('@fullhuman/postcss-purgecss')({
+    purgeCSSPlugin({
       content: [
         './src/index.html',
         './src/index.jsx',
